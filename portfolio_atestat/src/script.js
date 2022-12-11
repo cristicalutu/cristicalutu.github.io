df24a4ba9d355de183c23c362c50b5891f2a79af
cristicalutu <84141357+cristicalutu@users.noreply.github.com>
2022-12-11 15:25:38 +0200

    Add files via upload

--- portfolio_atestat/src/script.js
+++ portfolio_atestat/src/script.js
@@ -1,24 +1,107 @@
-//Navigation bar effects on scroll
-window.addEventListener("scroll", function()
-{
-    const header = this.document.querySelector("header");
-    header.classList.toggle("sticky", this.window.scrollY > 0);
-})
 
-//Portfolio section - Modal
+const menu = document.getElementById("menu");
 
-//Our clients - Swiper
+Array.from(document.getElementsByClassName("menu-item"))
+  .forEach((item, index) => {
+    item.onmouseover = () => {
+      menu.dataset.activeIndex = index;
+    }
+  });
 
-//Website dark/light theme
+//magic
 
-//Scroll to top button
+let index = 0,
+    interval = 1000;
 
-//Navigation menu items active on page scroll
+const rand = (min, max) => 
+  Math.floor(Math.random() * (max - min + 1)) + min;
 
-//Responsive navigation menu toggle
+const animate = star => {
+  star.style.setProperty("--star-left", `${rand(-10, 100)}%`);
+  star.style.setProperty("--star-top", `${rand(-40, 80)}%`);
 
-//Scroll reveal animations
-//Common reveal options to create reveal animations
+  star.style.animation = "none";
+  star.offsetHeight;
+  star.style.animation = "";
+}
 
-//Target elements, and specify options to create reveal animations
-      
+for(const star of document.getElementsByClassName("magic-star")) {
+  setTimeout(() => {
+    animate(star);
+    
+    setInterval(() => animate(star), 1000);
+  }, index++ * (interval / 3))
+}
+
+//scroll animations
+
+const observer = new IntersectionObserver((entries) =>
+  entries.forEach((entry) =>
+  {
+    console.log(entry)
+    if(entry.isIntersecting)
+    {
+      entry.target.classList.add('show');
+    }
+  })
+);
+
+const homeElements = document.querySelectorAll('.home');
+homeElements.forEach((el) => observer.observe(el));
+
+
+/* -- Carousel Navigation -- */
+
+let activeIndex = 0;
+
+const slides = document.getElementsByTagName("article");
+
+const handleLeftClick = () => {
+  const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : slides.length - 1;
+  
+  const currentSlide = document.querySelector(`[data-index="${activeIndex}"]`),
+        nextSlide = document.querySelector(`[data-index="${nextIndex}"]`);
+        
+  currentSlide.dataset.status = "after";
+  
+  nextSlide.dataset.status = "becoming-active-from-before";
+  
+  setTimeout(() => {
+    nextSlide.dataset.status = "active";
+    activeIndex = nextIndex;
+  });
+}
+
+const handleRightClick = () => {
+  const nextIndex = activeIndex + 1 <= slides.length - 1 ? activeIndex + 1 : 0;
+  
+  const currentSlide = document.querySelector(`[data-index="${activeIndex}"]`),
+        nextSlide = document.querySelector(`[data-index="${nextIndex}"]`);
+  
+  currentSlide.dataset.status = "before";
+  
+  nextSlide.dataset.status = "becoming-active-from-after";
+  
+  setTimeout(() => {
+    nextSlide.dataset.status = "active";
+    activeIndex = nextIndex;
+  });
+}
+
+/* -- Mobile Nav Toggle -- */
+
+const nav = document.querySelector("nav");
+
+const handleNavToggle = () => {  
+  nav.dataset.transitionable = "true";
+  
+  nav.dataset.toggled = nav.dataset.toggled === "true" ? "false" : "true";
+}
+
+window.matchMedia("(max-width: 800px)").onchange = e => {
+  nav.dataset.transitionable = "false";
+
+  nav.dataset.toggled = "false";
+};
+
+//work
